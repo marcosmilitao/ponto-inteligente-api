982e663af6bd35517cea1aeb6fc55c2632417f3a
marcos <marcosxr7@gmail.com>
2019-03-26 11:22:19 -0300

    Adicionados Enums e Entities

---
+++ src/main/java/com/militao/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,7 @@
+package com.militao.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+
+	ROLE_ADMIN,
+	ROLE_USUARIO
+}
---
+++ src/main/java/com/militao/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.militao.pontointeligente.api.enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABAHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+}
